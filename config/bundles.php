335d95b564f32fae9f00151eb4f0deee8555d6d7
Julien Robic <nayte91@gmail.com>
2020-06-30 20:49:19 +0200

    Added vich/uploader-bundle to try it out.

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
