b93fe995345100b00651b7924c987fd24cac858f
Julien Robic <nayte91@gmail.com>
2020-08-07 18:25:08 +0200

    FRONT : Added "slick" sliders to make a slider for images & videos when there is more than 4 of them.

--- public/assets/js/medias.js
+++ public/assets/js/medias.js
@@ -4,12 +4,24 @@ function listPictures($id, editable) {
 
         if (Object.keys($pictures).length === 0 ) {
             $('#picturesAndVideos').append("<p class='text-center'>No picture or video yet.</p>");
+            return;
         }
 
         $.each( $pictures, function( i, $picture ) {
             console.log($picture);
             var path = window.location.protocol+"//"+window.location.host+"/"+$picture.webPath;
-            $('#picturesAndVideos').append("<img src='"+path+"' alt='"+$picture.alt+"' height=auto class='col-2'>");
+            $('#picturesAndVideos').append("<img src='"+path+"' alt='"+$picture.alt+"' height=auto class='col-3'>");
         });
+
+        if (Object.keys($pictures).length > 4 ) {
+            $('.demo').slick({
+                dots: false,
+                infinite: false,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                prevArrow: '<button type="button" class="slick-prev rounded-circle btn btn-danger"><i class="fas fa-arrow-left"></i></button>',
+                nextArrow: '<button type="button" class="slick-next rounded-circle btn btn-danger"><i class="fas fa-arrow-right"></i></button>'
+            });
+        }
     });
 }
